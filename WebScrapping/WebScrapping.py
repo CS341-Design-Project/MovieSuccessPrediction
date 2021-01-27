31963a378d741277ae98c57f0b7197069c5d0e91
jeswin99 <69343208+jeswin99@users.noreply.github.com>
2021-01-27 08:27:05 +0530

    Added function rating_google

--- WebScrapping/WebScrapping.py
+++ WebScrapping/WebScrapping.py
@@ -13,3 +13,33 @@ headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
+
+def rating_google(title) :
+  try :
+    url = "https://www.google.com/search?q="+title
+    r = requests.get(url,headers=headers)
+    soup = BeautifulSoup(r.text, 'lxml')
+
+    google = soup.find_all('div', attrs={'class':'srBp4 Vrkhme'})
+    imdb = soup.find_all('span', attrs={'class':'gsrt IZACzd'})
+
+    if len(google) :
+      rating = google[0].contents[0][0:2]
+
+    elif len(imdb):
+        if (imdb[0].contents[0][1] != '/'):
+          rating = float(imdb[0].contents[0][0:3])*10
+        else :
+          rating = float(imdb[0].contents[0][0])*10
+    
+    else :
+      rating = 0
+    
+    time.sleep(2)
+
+    return int(rating)
+
+  except :
+    print("Couldn't fetch rating for " + title)
+    rating = 0
+    return int(rating)
